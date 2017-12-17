87d229d3767e009fd2bcd35bda1f69f5278d5e9d
Marcin Dzioch <marcindzioch@gmail.com>
2017-12-17 17:42:38 +0100

    update, select robot

--- src/GuiApp.java
+++ src/GuiApp.java
@@ -2,31 +2,51 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+
 public class GuiApp extends JFrame {
 
     private JPanel panelMain;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
+    private JButton rightButton;
+    private JButton stopButton;
+    private JButton buttonUp;
+    private JButton leftButton;
+    private JButton buttonDown;
     private JButton ustawButton;
-    private JButton wybierzButton;
     private JTextField textField1;
     private JPanel vid1;
     private JPanel vid2;
     private JPanel vid3;
     private JPanel vid4;
-    private JButton button6;
+    private JButton getDataButton;
     private JButton a270Button;
     private JButton a90Button;
     private JButton a180Button;
     private JButton a360Button;
     private JPanel IconPanel;
     private JLabel Connection;
+    private JLabel robotControlLabel;
+    private JButton openDataButton;
     private PlayerPanel v1, v2, v3, v4;
     private Client c;
-   private List<Client> clientList;
+    private List<Client> clientList;
+    private String rozkaz;
+
+    public JLabel getRobotControlLabel() {
+        return robotControlLabel;
+    }
+
+    public void setRobotControlLabel(JLabel robotControlLabel) {
+        this.robotControlLabel = robotControlLabel;
+    }
+
+    public String getRozkaz() {
+        return rozkaz;
+    }
+
+    public void setRozkaz(String rozkaz) {
+        this.rozkaz = rozkaz;
+    }
+
     ArrayList<JLabel> listofLabels = new ArrayList<JLabel>(100);
 
 
@@ -44,38 +64,29 @@ public class GuiApp extends JFrame {
         vid1.add(v1 = new PlayerPanel());
         vid2.setLayout(new BorderLayout());
         vid2.add(v2 = new PlayerPanel());
-         vid3.setLayout(new BorderLayout());
-         vid3.add(v3 = new PlayerPanel());
-         vid4.setLayout(new BorderLayout());
-         vid4.add(v4 = new PlayerPanel());
+        vid3.setLayout(new BorderLayout());
+        vid3.add(v3 = new PlayerPanel());
+        vid4.setLayout(new BorderLayout());
+        vid4.add(v4 = new PlayerPanel());
         //pack();
 
         setVisible(true);
 
         ////v1.play("http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4");
-     //  v2.play("http://vjs.zencdn.net/v/oceans.mp4");
-     //  v3.play("http://vjs.zencdn.net/v/oceans.mp4");
-      //  v4.play("http://vjs.zencdn.net/v/oceans.mp4");
+        //  v2.play("http://vjs.zencdn.net/v/oceans.mp4");
+        //  v3.play("http://vjs.zencdn.net/v/oceans.mp4");
+        //  v4.play("http://vjs.zencdn.net/v/oceans.mp4");
+
     }
 
     /**
      * @return the menu factory instance
      */
-    private MenuFactory getMenuFactory()
-    {
-        if (this.menuFactory == null)
-        {
+    private MenuFactory getMenuFactory() {
+        if (this.menuFactory == null) {
             menuFactory = new MenuFactory();
         }
         return this.menuFactory;
-    }
-
-    public JButton getWybierzButton() {
-        return wybierzButton;
-    }
-
-    public void setWybierzButton(JButton wybierzButton) {
-        this.wybierzButton = wybierzButton;
     }
 
     public JLabel getConnection() {
@@ -86,48 +97,41 @@ public class GuiApp extends JFrame {
     /**
      * Creates menu bar
      */
-    private void createMenuBar()
-    {
+    private void createMenuBar() {
 
-       final JMenuBar menuBar = new JMenuBar();
-       final MenuFactory menuFactory = getMenuFactory();
-       menuBar.add(menuFactory.getHelpMenu(this));
+        final JMenuBar menuBar = new JMenuBar();
+        final MenuFactory menuFactory = getMenuFactory();
+        menuBar.add(menuFactory.getHelpMenu(this));
 
-       setJMenuBar(menuBar);
+        setJMenuBar(menuBar);
+
+    }
+
+    public void changeconnectionIcon(JLabel icon, JRadioButton jRadioButton, Client client, List<Client> clientList) {
+
+        if (client.isConnected()) {
+            ImageIcon imgThisImg = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("image/connected-256.png")));
+            IconPanel.add(jRadioButton);
+            IconPanel.add(icon);
+            jRadioButton.setText("Robot: " + client.getIp());
+            icon.setIcon(imgThisImg);
+
+        } else {
+            jRadioButton.setVisible(false);
+            icon.setVisible(false);
+            IconPanel.repaint();
+
+            if (clientList.size() == 0) {
+                //ImageIcon imgThisImg = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("image/disconnected-256.png")));
+                getConnection().setVisible(true);
+                robotControlLabel.setVisible(false);
+                IconPanel.repaint();
+            }
+
+        }
 
     }
 
-    public void changeconnectionIcon ( JLabel icon, JRadioButton jRadioButton,Client client, List<Client> clientList)
-    {
-
-if (client.isConnected()) {
-    ImageIcon imgThisImg = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("image/connected-256.png")));
-    IconPanel.add(jRadioButton);
-    IconPanel.add(icon);
-
-
-    jRadioButton.setText("Robot: "+ client.getIp());
-    icon.setIcon(imgThisImg);
-}
-else
-{
-    //icon.setIcon(imgThisImg);
-    jRadioButton.setVisible(false);
-    icon.setVisible(false);
-    IconPanel.repaint();
-
-
-    if (clientList.size()==0)
-    {
-        //ImageIcon imgThisImg = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("image/disconnected-256.png")));
-        getConnection().setVisible(true);
-        getWybierzButton().setVisible(false);
-        IconPanel.repaint();
-    }
-
-}
-
-    }
     /**
      * Sets initial size on startup
      */
@@ -141,11 +145,28 @@ else
         // For screenshots only -> setBounds(50, 50, 850, 650);
     }
 
+    public JButton getButtonUp() {
+        return buttonUp;
+    }
+
+    public void setButtonUp(JButton buttonUp) {
+        this.buttonUp = buttonUp;
+    }
+
     /**
      * Set title and icon
      */
     private void decorateFrame() {
         setTitle("Khepera IV control panel");
         setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("image/logo.png")));
+
+    }
+
+    public JButton getButtonDown() {
+        return buttonDown;
+    }
+
+    public void setButtonDown(JButton buttonDown) {
+        this.buttonDown = buttonDown;
     }
 }
--- src/TCPServer.java
+++ src/TCPServer.java
@@ -3,6 +3,8 @@
 
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -28,9 +30,9 @@ public class TCPServer {
                 //System.out.println("Client connect" + clientSocket.getLocalAddress());
                 System.out.println("Connection received from " + clientSocket.getInetAddress().getHostName() + " : " + clientSocket.getPort());
 
-                Client cc= new Client(clientSocket.getInetAddress().toString(), clientSocket.getPort(), clientSocket.getInetAddress().getHostName(), clientList.size(), true);
+                Client cc = new Client(clientSocket.getInetAddress().toString(), clientSocket.getPort(), clientSocket.getInetAddress().getHostName(), clientList.size(), true);
                 clientList.add(cc);
-                Connection c = new Connection(clientSocket, clientList,gui,cc );
+                Connection c = new Connection(clientSocket, clientList, gui, cc);
 
             }
         } catch (IOException e) {
@@ -50,16 +52,18 @@ class Connection extends Thread {
     Socket clientSocket;
     int port;
     Client cc;
-    JLabel jlabel=new JLabel();
-   JRadioButton jRadioButton= new JRadioButton();
+    JLabel jlabel = new JLabel();
+    JRadioButton jRadioButton = new JRadioButton();
+    String rozkaz = null;
 
-    public Connection(Socket aClientSocket, List<Client> clientList, GuiApp gui,Client cc) {
+    public Connection(Socket aClientSocket, List<Client> clientList, GuiApp gui, Client cc) {
         this.clientList = clientList;
-        this.gui=gui;
-        this.cc=cc;
-gui.getWybierzButton().setVisible(true);
-gui.getConnection().setVisible(false);
-        gui.changeconnectionIcon(jlabel,jRadioButton,cc,clientList);
+        this.gui = gui;
+        this.cc = cc;
+        gui.getRobotControlLabel().setVisible(true);
+        gui.getConnection().setVisible(false);
+
+        gui.changeconnectionIcon(jlabel, jRadioButton, cc, clientList);
         String listString = "";
 
         for (Client client : clientList) {
@@ -90,64 +94,57 @@ gui.getConnection().setVisible(false);
             red = clientSocket.getInputStream().read(buffer);
 
 
-
-            if (red==-1)
-            {
+            if (red == -1) {
                 cc.setConnected(false);
                 return;
             }
-    redData = new byte[red];
-
+            redData = new byte[red];
 
 
             //wyjatek
             System.arraycopy(buffer, 0, redData, 0, red);
             redDataText = new String(redData, "UTF-8"); // assumption that client sends data UTF-8 encoded
-            System.out.println("Client :" + redDataText);
+            System.out.println("Client " + clientSocket.getInetAddress().getHostName() + " : " + clientSocket.getPort() +": " + redDataText);
             clientData.append(redDataText);
 
         } catch (IOException | NegativeArraySizeException e) {
             System.out.println("error");
 
 
-
-
-
             removeClient();
 
-        try {
-            clientSocket.close();
-        } catch (IOException e1) {
-            e1.printStackTrace();
+            try {
+                clientSocket.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            e.printStackTrace();
         }
-        e.printStackTrace();
-    }
-    // System.out.println("odczytano, Data From Client: " + clientData.toString());
+        // System.out.println("odczytano, Data From Client: " + clientData.toString());
 
         if (clientData.toString().equals("stop")) {
-        cc.setConnected(false);
+            cc.setConnected(false);
+        }
+
     }
 
-}
-
-    public synchronized void removeClient()
-    {
+    public synchronized void removeClient() {
         cc.setConnected(false);
-      //  System.out.println(cc.isConnected());
+        //  System.out.println(cc.isConnected());
 ///////////
 
         Iterator<Client> i = clientList.iterator();
         while (i.hasNext()) {
             Client s = i.next(); // must be called before you can call i.remove()
             // Do something
-            if (s.getPort()==port)
-            i.remove();
+            if (s.getPort() == port)
+                i.remove();
         }
 
         ///////////////    if (client.getPort() == port)
 
         //System.out.print("dadsadasdsadsa"+ clientList.size());
-        gui.changeconnectionIcon(jlabel,jRadioButton,cc,clientList);
+        gui.changeconnectionIcon(jlabel, jRadioButton, cc, clientList);
     }
 
     public void sendToOneClient(String rozkaz) {
@@ -166,23 +163,62 @@ gui.getConnection().setVisible(false);
             e.printStackTrace();
         }
 
-        System.out.println("Server replay: "+rozkaz);
+        System.out.println("Server to client " + clientSocket.getInetAddress().getHostName() + " : " + clientSocket.getPort() + ": " + rozkaz);
+    }
+
+    public String getRozkaz() {
+        return rozkaz;
+    }
+
+    public void setRozkaz(String rozkaz) {
+        this.rozkaz = rozkaz;
+    }
+
+
+    public void waitfor() {
+        rozkaz = null;
+        gui.getButtonUp().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setRozkaz("up");
+
+            }
+        });
+        gui.getButtonDown().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setRozkaz("down");
+
+            }
+        });
     }
 
     public void run() {
-        //////////
+
         port = clientSocket.getPort();
         while (cc.isConnected()) {
 
 
 
-            sendToOneClient("t");
 
+            rozkaz = null;
+            while (rozkaz == null) {
 
+                waitfor();
+                try {
+                    sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
 
+            while (!jRadioButton.isSelected() == true) {
+                System.out.println("wybierz robota");
+            }
+            sendToOneClient(rozkaz);
             readFromClient(cc);
 
-            //////////
+
 
             //  try { // an echo server
             //  String data = input.readUTF();
@@ -221,21 +257,16 @@ gui.getConnection().setVisible(false);
 
 */
 
-           /// if (clientSocket.isClosed() || !clientSocket.isConnected()) {
+            /// if (clientSocket.isClosed() || !clientSocket.isConnected()) {
 
 
-
-
-           // }
+            // }
 
 
         }
-        System.out.println("Client "+ clientSocket.getInetAddress().getHostName() + " : " + clientSocket.getPort() + " disconnect");
+        System.out.println("Client " + clientSocket.getInetAddress().getHostName() + " : " + clientSocket.getPort() + " disconnect");
         removeClient();
         stop();
-
-
-
 
 
         try {
@@ -248,6 +279,3 @@ gui.getConnection().setVisible(false);
 
     }
 }
-
-
-
